1a8adcc8b7e41291faca1970dd450d3a977e57bc
Alex <21maasarani@mail.ru>
2022-04-27 00:02:43 +0300

    Я лучший бля

--- js/app.js
+++ js/app.js
@@ -15,27 +15,25 @@ let colorBlack = '#000';
 function getWeightAdvice() {
     let w1 = document.getElementById("uW").value;
 
-
     if (w1 <= 40) {
-        document.getElementById("res").innerHTML = "Вам необходимо пройти программу по набору массы";
-        document.getElementById("res").style.color = colorBlue;
-        document.getElementById("res").style.fontSize = '30px';
-        document.getElementById("res").style.fontFamily = 'serif';
+        fitAdviсe("Вам необходимо пройти программу по набору массы", colorBlue, '30px', 'serif');
     } else if (w1 <= 95) {
-        document.getElementById("res").innerHTML = "Ваш вес соответствует требованиям";
-        document.getElementById("res").style.color = colorGreen;
-        document.getElementById("res").style.fontSize = '25px';
-        document.getElementById("res").style.fontFamily = 'sans-serif';
+        fitAdviсe("Ваш вес соответствует требованиям", colorGreen, '25px', 'sans-serif');
     } else if (w1 >= 96) {
-        document.getElementById("res").innerHTML = "К сожалению вес привышает допустимое значение. Необходимо пройти программу по сброссу массы";
-        document.getElementById("res").style.color = colorRed;
-        document.getElementById("res").style.fontSize = '15px';
-        document.getElementById("res").style.fontFamily = 'serif';
+        fitAdviсe("К сожалению вес привышает допустимое значение. Необходимо пройти программу по сброссу массы", colorRed, '15px', 'serif');
     } else {
-        document.getElementById("res").innerHTML = "Введены неверные данные";
-        document.getElementById("res").style.color = colorBlack;
-        document.getElementById("res").style.fontSize = '10px';
-        document.getElementById("res").style.fontFamily = 'sans-serif';
+        fitAdviсe("Введены неверные данные", colorBlack, '10px', 'sans-serif');
     }
     
 }
+
+function fitAdviсe (message, color, fontsize, fontfamily) {
+    
+    document.getElementById("res").innerHTML = message;
+    document.getElementById("res").style.color = color;
+    document.getElementById("res").style.fontSize = fontsize;
+    document.getElementById("res").style.fontFamily = fontfamily;
+}
+
+
+
