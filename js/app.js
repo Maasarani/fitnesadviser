1e071e273e5424f90d6642bbd347d1caea8eacc0
Alex <21maasarani@mail.ru>
2022-04-27 00:12:40 +0300

    Провел окончательый рефакторинг

--- js/app.js
+++ js/app.js
@@ -2,38 +2,32 @@
 
 document.getElementById("go").addEventListener("click", getWeightAdvice);
 
+const COLOR_BLUE = '#1fddff';
+const COLOR_GREEN = '#00bf8f';
+const COLOR_RED = '#F11712';
+const COLOR_BLACK = '#000';
 
-// 1. Повторяющиеся данные добавляем в переменные 
-// 2. Изменить название переменных, на более осмысленные
-
-let colorBlue = '#1fddff';
-let colorGreen = '#00bf8f';
-let colorRed = '#F11712';
-let colorBlack = '#000';
-
-
-function getWeightAdvice() {
-    let w1 = document.getElementById("uW").value;
-
-    if (w1 <= 40) {
-        fitAdviсe("Вам необходимо пройти программу по набору массы", colorBlue, '30px', 'serif');
-    } else if (w1 <= 95) {
-        fitAdviсe("Ваш вес соответствует требованиям", colorGreen, '25px', 'sans-serif');
-    } else if (w1 >= 96) {
-        fitAdviсe("К сожалению вес привышает допустимое значение. Необходимо пройти программу по сброссу массы", colorRed, '15px', 'serif');
-    } else {
-        fitAdviсe("Введены неверные данные", colorBlack, '10px', 'sans-serif');
-    }
-    
-}
-
-function fitAdviсe (message, color, fontsize, fontfamily) {
-    
+function displayMessage (message, color, fontsize, fontfamily) {
     document.getElementById("res").innerHTML = message;
     document.getElementById("res").style.color = color;
     document.getElementById("res").style.fontSize = fontsize;
     document.getElementById("res").style.fontFamily = fontfamily;
 }
 
+function getWeightAdvice() {
+    let weightValue = document.getElementById("uW").value;
+
+    if (weightValue <= 40) {
+        displayMessage("Вам необходимо пройти программу по набору массы", COLOR_BLUE, '30px', 'serif');
+    } else if (weightValue <= 95) {
+        displayMessage("Ваш вес соответствует требованиям", COLOR_GREEN, '25px', 'sans-serif');
+    } else if (weightValue >= 96) {
+        displayMessage(`К сожалению вес привышает допустимое значение. 
+        Необходимо пройти программу по сброссу массы`, COLOR_RED, '15px', 'serif');
+    } else {
+        displayMessage("Введены неверные данные", COLOR_BLACK, '10px', 'sans-serif');
+    }
+    
+}
 
 
