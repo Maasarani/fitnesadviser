ddc7f2ff950ebfbe8405e88ef8f00c02662628b5
Alex <21maasarani@mail.ru>
2022-04-26 22:51:40 +0300

    Добавлены разные шрифты для сообщений

--- js/app.js
+++ js/app.js
@@ -20,18 +20,22 @@ function getWeightAdvice() {
         document.getElementById("res").innerHTML = "Вам необходимо пройти программу по набору массы";
         document.getElementById("res").style.color = colorBlue;
         document.getElementById("res").style.fontSize = '30px';
+        document.getElementById("res").style.fontFamily = 'serif';
     } else if (w1 <= 95) {
         document.getElementById("res").innerHTML = "Ваш вес соответствует требованиям";
         document.getElementById("res").style.color = colorGreen;
         document.getElementById("res").style.fontSize = '25px';
+        document.getElementById("res").style.fontFamily = 'sans-serif';
     } else if (w1 >= 96) {
         document.getElementById("res").innerHTML = "К сожалению вес привышает допустимое значение. Необходимо пройти программу по сброссу массы";
         document.getElementById("res").style.color = colorRed;
         document.getElementById("res").style.fontSize = '15px';
+        document.getElementById("res").style.fontFamily = 'serif';
     } else {
         document.getElementById("res").innerHTML = "Введены неверные данные";
         document.getElementById("res").style.color = colorBlack;
         document.getElementById("res").style.fontSize = '10px';
+        document.getElementById("res").style.fontFamily = 'sans-serif';
     }
     
 }
